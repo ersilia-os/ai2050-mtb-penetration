730cf121e2095b82dc8a7f78aefb353fa446ed8e
GemmaTuron <gemma@ersilia.io>
2025-10-29 17:17:11 +0100

    add models

---
+++ scripts/lazybinarypredictor.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import pandas as pd
+from lazyqsar.qsar import LazyBinaryQSAR
+
+
+root = os.path.dirname(os.path.abspath(__file__))
+
+dataset = sys.argv[1]
+models = ["sarathy2016", "janardhan2016", "radchenko2023", "lepori2025_mtb", "lepori2025_msm"]
+pred_file = os.path.join(root, "..","data", "raw", f"{dataset}.csv")
+
+df = pd.read_csv(pred_file)
+x_test = df["smiles"].tolist()
+
+for m in models:
+    model_folder = os.path.join(root, "..", "models", f"{m}")
+    model = LazyBinaryQSAR.load(model_folder)
+    y_pred = model.predict_proba(smiles_list=x_test)[:, 1]
+    df[f"{m}"] = y_pred
+
+df.to_csv(os.path.join(root, "..","output", "results", f"{dataset}.csv"), index=False)
+
---
+++ scripts/run.sh
@@ -0,0 +1,1 @@
+python lazybinaryclassifier.py radchenko2023
